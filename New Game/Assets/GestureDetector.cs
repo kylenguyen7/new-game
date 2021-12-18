dd377bbb00643d0899fa34c7d8d43ce1e25b846a
Kyle <kylenguyen7777@gmail.com>
2021-12-18 12:27:25 -0500

    Added gesture detector and new character art.

---
+++ New Game/Assets/GestureDetector.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using _Common;
+using Unity.Mathematics;
+using UnityEditor;
+using UnityEngine;
+
+public class GestureDetector : MonoBehaviour {
+    [SerializeField] private float _minGestureLength;
+    [SerializeField] private LineRenderer _lineRenderer;
+    private bool _gesturing;
+    private Vector2 _start;
+    private Vector3[] _emptyPositions = { Vector3.zero, Vector3.zero };
+
+    private void Awake() {
+        _lineRenderer.SetPositions(_emptyPositions);
+    }
+
+    private void Update() {
+        if (Input.GetMouseButtonDown(1)) {
+            _start = Input.mousePosition;
+            _lineRenderer.SetPosition(0, KaleUtils.GetMousePosWorldCoordinates());
+            _gesturing = true;
+        }
+
+        if (_gesturing) {
+            // Update indicator
+            _lineRenderer.SetPosition(1, KaleUtils.GetMousePosWorldCoordinates());
+            Vector2 gesture = _start - (Vector2)Input.mousePosition;
+            _lineRenderer.startColor = _lineRenderer.endColor = gesture.magnitude >= _minGestureLength ? Color.yellow : Color.gray;
+            
+            // End gesture
+            if (Input.GetMouseButtonUp(1)) {
+                if (gesture.magnitude >= _minGestureLength) {
+                    Debug.Log(gesture.normalized);
+                }
+                _lineRenderer.SetPositions(_emptyPositions);
+                _gesturing = false;
+            }
+        }
+    }
+}
