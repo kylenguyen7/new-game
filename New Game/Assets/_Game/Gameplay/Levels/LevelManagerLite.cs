31d64c58487a8825de01fa0f523cae7e8fc6e327
Kyle <kylenguyen7777@gmail.com>
2021-12-31 22:37:09 -0500

    Added basic town/dungeon portal loop.

--- New Game/Assets/_Game/Gameplay/Levels/LevelManagerLite.cs
+++ New Game/Assets/_Game/Gameplay/Levels/LevelManagerLite.cs
@@ -4,32 +4,58 @@ using UnityEngine.SceneManagement;
 using Random = UnityEngine.Random;
 
 public class LevelManagerLite : MonoBehaviour {
+    public static LevelManagerLite Instance;
+    
     [SerializeField] private GameObject _spawnerPrefab;
     [SerializeField] private List<Level> _levels;
-
-    private Level _level;
+    [SerializeField] private int _levelsPerPortal;
+    
     private SpawnerController _spawner;
+    private Level _level;
+    private int _levelCount;
 
     private void Awake() {
-        _level = _levels[Random.Range(0, _levels.Count)];
+        if (Instance != null) {
+            Destroy(gameObject);
+            return;
+        }
         
-        // Create spawner and set spawn bounds
-        _spawner = Instantiate(_spawnerPrefab, Vector3.zero, Quaternion.identity).GetComponent<SpawnerController>();
-        _spawner.SetSpawnSize(_level.CameraBounds.x - 4, _level.CameraBounds.y - 4);
-        
-        // Create level
-        Instantiate(_level.LevelPrefab, Vector3.zero, Quaternion.identity);
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += OnSceneLoaded;
     }
+    
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
+        // If combat scene, start spawn sequence and reset counter
+        if (scene.buildIndex == 0) {
+            _level = _levels[Random.Range(0, _levels.Count)];
+        
+            // Create spawner and set spawn bounds
+            _spawner = Instantiate(_spawnerPrefab, Vector3.zero, Quaternion.identity).GetComponent<SpawnerController>();
+            _spawner.SetSpawnSize(_level.CameraBounds.x - 4, _level.CameraBounds.y - 4);
+        
+            // Create level
+            Instantiate(_level.LevelPrefab, Vector3.zero, Quaternion.identity);
+            
+            // Set camera bounds
+            CameraController.Instance.SetCameraSize(_level.CameraBounds);
 
-    private void Start() {
-        // Set camera bounds
-        CameraController.Instance.SetCameraSize(_level.CameraBounds);
+            _levelCount += 1;
+        } else if (scene.buildIndex == 1) {
+            _levelCount = 0;
+        }
+        
+        Debug.Log($"Scene loaded. Level count is {_levelCount}.");
     }
 
     private void Update() {
         // When spawner has finished last wave
-        if (_spawner.Finished && Input.GetKeyDown(KeyCode.LeftControl)) {
-            SceneManager.LoadScene(0);
+        if (_spawner && _spawner.Finished && Input.GetKeyDown(KeyCode.LeftControl)) {
+            if (_levelCount == _levelsPerPortal) {
+                SceneManager.LoadScene(1);
+            } else {
+                SceneManager.LoadScene(0);
+            }
         }
     }
 }
