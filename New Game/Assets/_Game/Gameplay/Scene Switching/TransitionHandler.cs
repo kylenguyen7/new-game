f04a01b0f267068c02ec9522d17c07c8f8e184fc
Kyle <kylenguyen7777@gmail.com>
2022-01-12 22:52:08 -0800

    Added basic fade in/out transition between scenes.

--- New Game/Assets/_Game/Gameplay/Scene Switching/TransitionHandler.cs
+++ New Game/Assets/_Game/Gameplay/Scene Switching/TransitionHandler.cs
@@ -1,12 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TransitionHandler : MonoBehaviour {
     public static TransitionHandler Instance;
+    [SerializeField] private Image blackScreen;
+    [SerializeField] private float fadeTime;
+    [SerializeField] private float secondsPerFadeTick;
+
+    private Coroutine _currentTransition;
+    
     private void Awake() {
         if (Instance != null) {
             Destroy(gameObject);
@@ -14,13 +21,46 @@ public class TransitionHandler : MonoBehaviour {
         }
 
         Instance = this;
+
+        if (!blackScreen.enabled) {
+            blackScreen.enabled = true;
+        }
+        Color col = blackScreen.color;
+        col.a = 0;
+        blackScreen.color = col;
     }
 
     public void SaveSceneAndLoadNewScene(String sceneName) {
+        if (_currentTransition != null) {
+            Debug.LogError($"Cannot transition to {sceneName} while another transition is occurring!");
+            return;
+        }
+        
+        _currentTransition = StartCoroutine(TransitionSequence(sceneName));
+    }
+
+    private IEnumerator TransitionSequence(String sceneName) {
+        Time.timeScale = 0f;
         foreach (var saveable in FindObjectsOfType<Saveable>()) {
             saveable.Save();
         }
-        
+
+        Color col = blackScreen.color;
+        for (float alpha = 0; alpha < 1f; alpha += secondsPerFadeTick / fadeTime) {
+            col.a = alpha;
+            blackScreen.color = col;
+            yield return new WaitForSecondsRealtime(secondsPerFadeTick);
+        }
+
         SceneManager.LoadScene(sceneName);
+        
+        for (float alpha = 1; alpha > 0; alpha -= secondsPerFadeTick / fadeTime) {
+            col.a = alpha;
+            blackScreen.color = col;
+            yield return new WaitForSecondsRealtime(secondsPerFadeTick);
+        }
+        
+        Time.timeScale = 1;
+        _currentTransition = null;
     }
 }
