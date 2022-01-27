06d976290b78b3fc2c44140983cb4d9d74cb7511
Kyle <kylenguyen7777@gmail.com>
2022-01-26 20:36:10 -0800

    Implemented basic color reading utility.

---
+++ New Game/Assets/_Game/Gameplay/Procedural Generation/ImageReader.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Drawing;
+
+public static class ImageReader {
+    public static Color[,] ReadImage(String path) {
+        Bitmap img = new Bitmap(path);
+
+        Color[,] colors = new Color[img.Width, img.Height];
+        for (int y = 0; y < img.Height; y++) {
+            for (int x = 0; x < img.Width; x++) {
+                colors[x, y] = img.GetPixel(x, y);
+            }
+        }
+        
+        return colors;
+    }
+}
