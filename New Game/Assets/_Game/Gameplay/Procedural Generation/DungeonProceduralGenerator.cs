be1f603553ed1c36fca0d761d89593cc8601dfe3
Kyle <kylenguyen7777@gmail.com>
2022-02-01 21:58:24 -0800

    Began BoI style room placement algorithm.

--- New Game/Assets/_Game/Gameplay/Procedural Generation/DungeonProceduralGenerator.cs
+++ New Game/Assets/_Game/Gameplay/Procedural Generation/DungeonProceduralGenerator.cs
@@ -1,9 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DungeonProceduralGenerator : MonoBehaviour {
-    private static readonly int GRID_SIZE = 100;
+    private static readonly float RANDOM_GIVE_UP_CHANCE = 0f;
+    private static readonly int GRID_SIZE = 50;
+    private bool[,] dungeon = new bool[50, 50];
     
-    private bool[,] dungeon = new bool[GRID_SIZE, GRID_SIZE];
+    private Queue<Room> roomQueue = new Queue<Room>();
+    [SerializeField] private int roomsToGenerate = Random.Range(8, 10);
+    private int _roomsGenerated = 0;
+
+    private void GenerateRooms() {
+        // Enqueue initial room
+        roomQueue.Enqueue(new Room(GRID_SIZE / 2, GRID_SIZE / 2));
+        _roomsGenerated++;
+        
+        while (roomQueue.Count > 0) {
+            Room prevRoom = roomQueue.Dequeue();
+            // TODO: choose random neighbor of room
+        }
+    }
+
+    private bool CanPlaceRoom(Room room) {
+        int x = room.X;
+        int y = room.Y;
+        
+        // Position must be in bounds and not be occupied
+        if (!InBounds(x, y) || dungeon[x, y]) return false;
+        
+        // Position cannot have a filled neighbor
+        if (InBounds(x - 1, y) && dungeon[x - 1, y]) return false;
+        if (InBounds(x + 1, y) && dungeon[x + 1, y]) return false;
+        if (InBounds(x, y - 1) && dungeon[x, y - 1]) return false;
+        if (InBounds(x, y + 1) && dungeon[x, y + 1]) return false;
+
+        // Random give up
+        if (Random.value < RANDOM_GIVE_UP_CHANCE) return false;
+        
+        return true;
+    }
+    
+    private void PlaceRoom(int x, int y) {
+        dungeon[x, y] = true;
+        
+    }
+
+    private bool InBounds(int x, int y) {
+        return x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE;
+    }
 }
--- New Game/Assets/_Game/Gameplay/Procedural Generation/Room.cs
+++ New Game/Assets/_Game/Gameplay/Procedural Generation/Room.cs
@@ -3,47 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Room {
-    /*
-    private enum Direction {
-        UP,
-        LEFT,
-        RIGHT,
-        DOWN
-    }
-    */
+    public int X { get; }
+    public int Y { get; }
 
-    private List<Point> body;
-    // private Dictionary<Direction, List<Point>> walls = new Dictionary<Direction, List<Point>>();
-
-    public Room(List<Point> body) {
-        this.body = new List<Point>(body);
-    }
-    
-    private bool Place(bool[,] board, int x, int y) {
-        // Verify room can be placed
-        foreach (Point p in body) {
-            int pieceX = x + p.x;
-            int pieceY = y + p.y;
-            if (pieceX < 0 || pieceX >= board.GetLength(0) || pieceY < 0 || pieceY >= board.GetLength(1)) return false;
-            if (board[pieceX, pieceY]) return false;
-        }
-        
-        // Place room
-        foreach (Point p in body) {
-            int pieceX = x + p.x;
-            int pieceY = y + p.y;
-            board[pieceX, pieceY] = true;
-        }
-        return true;
-    }
-}
-
-public struct Point {
-    public int x;
-    public int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public Room(int x, int y) {
+        X = x;
+        Y = y;
     }
 }
